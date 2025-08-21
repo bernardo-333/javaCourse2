b595c921e3c74d47a7efd93e0617781927c7b7d2
bernardo-333 <bernardinho.freitas11@gmail.com>
2025-08-21 08:39:51 -0300

    feat: adicionei exercicio Main

---
+++ src/override/Main.java
@@ -0,0 +1,8 @@
+package override;
+
+public class Main {
+    public static void main(String[] args) {
+        Veiculo veiculo = new Veiculo("preto", "polo", "ADV2131");
+        Carro carro = new Carro("azul", "gol bola", "SAD13231");
+    }
+}
